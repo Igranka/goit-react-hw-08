1c17e033a6dcaf99d341fc23dc12e331f823d0d4
Igranka <sirbusiness0@gmail.com>
2024-09-22 19:56:07 +0300

    1.0.3

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,11 +1,11 @@
 import { createSelector } from "@reduxjs/toolkit";
-import { selectFilter } from "../filters/selectors";
+import { selectNameFilter } from "../filters/selectors";
 
 export const selectContacts = (state) => state.contacts.items;
 export const selectError = (state) => state.contacts.error;
 export const selectLoading = (state) => state.contacts.loading;
 export const selectFilteredContacts = createSelector(
-  [selectContacts, selectFilter],
+  [selectContacts, selectNameFilter],
   (contacts, filter) => {
     const data = contacts.filter((contact) =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
