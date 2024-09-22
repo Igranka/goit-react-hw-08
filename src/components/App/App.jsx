f22667f267af63b2d373325823e15f100ae2a3bb
Igranka <sirbusiness0@gmail.com>
2024-09-22 19:54:38 +0300

    1.0.2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,14 @@
 import { lazy, Suspense, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { refreshUser } from "../../redux/auth/operations";
+import { selectIsRefreshing } from "../../redux/auth/selectors";
 import Layout from "../../components/Layout/Layout";
 import RestrictedRoute from "../../components/Routes/RestrictedRoute";
 import PrivateRoute from "../../components/Routes/PrivateRoute";
 import Loader from "../../components/Loader/Loader";
 import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
-import { useDispatch, useSelector } from "react-redux";
-import { refreshUser } from "../../redux/auth/operations";
-import { selectIsRefreshing } from "../../redux/auth/selectors";
-
 
 const ContactsPage = lazy(() => import("../../pages/ContactsPage/ContactsPage"));
 const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,8 +1,9 @@
 import { useId } from "react";
 import css from "./SearchBox.module.css";
-
 import { useDispatch, useSelector } from "react-redux";
-import { changeFilter, selectNameFilter } from "../../redux/filters/slice";
+import { changeFilter } from "../../redux/filters/slice";
+import { selectNameFilter } from "../../redux/filters/selectors";
+
 
 const SearchBox = () => {
   const id = useId();
