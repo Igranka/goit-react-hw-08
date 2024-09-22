e21ad3f7f861fd8dcf8cfd75394846b1d23d18ee
Igranka <sirbusiness0@gmail.com>
2024-09-22 19:51:38 +0300

    1.0.1

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -2,7 +2,7 @@ import { useId } from "react";
 import css from "./SearchBox.module.css";
 
 import { useDispatch, useSelector } from "react-redux";
-import { changeFilter, selectNameFilter } from "../../redux/filtersSlice";
+import { changeFilter, selectNameFilter } from "../../redux/filters/slice";
 
 const SearchBox = () => {
   const id = useId();
